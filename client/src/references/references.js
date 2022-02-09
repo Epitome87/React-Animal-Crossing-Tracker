f457d8a5a8846c24a2147508b9c5ce8328b7288b
Matthew McGrath <rubywep@hotmail.com>
2022-02-08 23:06:29 -0800

    Add temp reference to store color palette.

---
+++ client/src/references/references.js
@@ -0,0 +1,6 @@
+// Animal Crossing: NH color palette
+// #9dffb0	(157,255,176)   Mint Green
+// #81f1f7	(129,241,247)   Sky Blue
+// #fffffa	(255,255,250)   White
+// #c48d3f	(196,141,63)    Brown
+// #fff563	(255,245,99)    Yellow
