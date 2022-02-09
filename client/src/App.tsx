8e7e1d0a35fc6d45ba0da10414b40bbb68e8a5a2
Matthew McGrath <rubywep@hotmail.com>
2022-02-08 23:14:56 -0800

    Add route for individual fish. Remove styling.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import FishScreen from './screens/FishScreen';
-import NotFoundScreen from './screens/NotFoundScreen';
+import HomeScreen from './screens/Home/HomeScreen';
+import FishScreen from './screens/Fish/FishScreen';
+import NotFoundScreen from './screens/NotFound/NotFoundScreen';
+import FishDetailScreen from './screens/Fish/FishDetailScreen';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomeScreen />} />
         <Route path='/fish' element={<FishScreen />} />
+        <Route path='/fish/:fishId' element={<FishDetailScreen /> } />
         <Route path='*' element={<NotFoundScreen />} />
       </Routes>
     </div>
