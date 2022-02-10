7bd8ee52a9523143c48e5b248170e299302e9928
Matthew McGrath <rubywep@hotmail.com>
2022-02-09 22:36:22 -0800

    Add more potential color scheme refs.

--- client/src/references/references.js
+++ client/src/references/references.js
@@ -4,3 +4,28 @@
 // #fffffa	(255,255,250)   White
 // #c48d3f	(196,141,63)    Brown
 // #fff563	(255,245,99)    Yellow
+
+
+/*
+Potential Color Schemes
+
+Home:
+
+
+Fish:
+#4d50f8 Primary Blue
+#81f1f7 Accent Blue
+
+Bug:
+#14342b Primary Green
+#7cae7a Accent Green
+
+Fossil:
+#4E2B16 Primary Brown
+##ECAD86 Accent Brown
+
+Villager:
+#8a3033 Primary Pink
+#dd99bb Accent Pink
+
+*/
