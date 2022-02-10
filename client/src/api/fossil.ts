a794a684ae9e88b05fbaab080cfc50f8670fe06d
Matthew McGrath <rubywep@hotmail.com>
2022-02-09 22:37:49 -0800

    Create temp dummy data for fossil collection.

---
+++ client/src/api/fossil.ts
@@ -0,0 +1,16 @@
+export const fossilData = [
+    {
+        "name": "acanthostega",
+        "price": 2000,
+        "museum_phrase":"The acanthostega! Said to be one of the earliest amphibians, it existed well before dinosaurs. Because they lived as fish not long before, they still had gills and very webbed \"hands.\". To toss away the life they knew and venture onto unknown lands... they must have been very brave! Hmm... Does it still count as bravery if you have no understanding of what you're doing?",
+        "image_uri": "https://acnhapi.com/v1/images/fossils/acanthostega",
+        "part_of": "acanthostega"
+    },
+    {
+        "name": "amber",
+        "price":1200,
+        "museum_phrase": "Amber is formed from the sap of ancient trees that hardened over time. Because of its beauty, it has often been traded and used as jewelry throughout history. However, individual specimens may contain ancient plants or insects trapped inside them! These are valuable resources for learning about ancient eras, such as when the dinosaurs roamed... And this is why they are sometimes displayed in certain...ahem... exceptional museums! Like mine.",
+        "image_uri": "https://acnhapi.com/v1/images/fossils/amber",
+        "part_of": "amber"
+    }
+]
